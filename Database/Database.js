3bf15424cf076006ab3094cc77505173463ad817
Ankit Mod <ankit300mod@gmail.com>
2026-01-29 15:11:33 +0530

    d

--- Database/Database.js
+++ Database/Database.js
@@ -12,13 +12,12 @@
 
 ///     MAIN CONNECTION CODE FOR HOSTINGER    ///
 
-
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  "u118145129_influencer_db",        // ✅ DB NAME
-  "u118145129_influencer_use",      // ✅ DB USER
-  "123!@#Aq",             // ✅ DB PASSWORD
+  "u118145129_influencer_db",
+  "u118145129_influencer_use",
+  "123!@#Aq",
   {
     host: "auth-db1132.hstgr.io",
     dialect: "mysql",
@@ -28,14 +27,18 @@ const sequelize = new Sequelize(
     pool: {
       max: 5,
       min: 0,
-      acquire: 30000,
+      acquire: 60000,
       idle: 10000,
     },
 
     dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
       connectTimeout: 60000,
     },
   }
 );
 
-module.exports = sequelize;   
+module.exports = sequelize;
