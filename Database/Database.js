25cbae0375db1091da1f87e71c5122857c367608
Ankit Mod <ankit300mod@gmail.com>
2026-01-29 15:38:52 +0530

    d

--- Database/Database.js
+++ Database/Database.js
@@ -1,43 +1,24 @@
 const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize(
-  "sql12815659",          // DB NAME
-  "sql12815659",          // DB USER
-  "4uPdKTG9Yd",           // DB PASSWORD
-  {
-    host: "sql12.freesqldatabase.com",
-    dialect: "mysql",
-    port: 3306,
-    logging: false,
-
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 60000,
-      idle: 10000,
-    },
-
-    dialectOptions: {
-      ssl: false,          // 🔴 FreeSQLDatabase DOES NOT support SSL
-      connectTimeout: 60000,
-    },
-  }
-);
-
+const sequelize = new Sequelize("Rishit_Project", "root", "root", {
+  host: "localhost",
+  dialect: "mysql",
+  port: 3306,
+  logging: false,
+});
 module.exports = sequelize;
-
 
 
 
 
 ///     MAIN CONNECTION CODE FOR HOSTINGER    ///
 
+
 // const { Sequelize } = require("sequelize");
 
 // const sequelize = new Sequelize(
-//   "u118145129_influencer_db",
-//   "u118145129_influencer_use",
-//   "123!@#Aq",
+//   "u118145129_influencer_db",        // ✅ DB NAME
+//   "u118145129_influencer_use",      // ✅ DB USER
+//   "123!@#Aq",             // ✅ DB PASSWORD
 //   {
 //     host: "auth-db1132.hstgr.io",
 //     dialect: "mysql",
@@ -47,18 +28,14 @@ module.exports = sequelize;
 //     pool: {
 //       max: 5,
 //       min: 0,
-//       acquire: 60000,
+//       acquire: 30000,
 //       idle: 10000,
 //     },
 
 //     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false,
-//       },
 //       connectTimeout: 60000,
 //     },
 //   }
 // );
 
-module.exports = sequelize;
+// module.exports = sequelize;   
