850babd06a174b2eef635b812394b25b4e7447b4
Ankit Mod <ankit300mod@gmail.com>
2026-01-29 15:05:08 +0530

    d

--- Database/Database.js
+++ Database/Database.js
@@ -1,11 +1,11 @@
-const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize("Rishit_Project", "root", "root", {
-  host: "localhost",
-  dialect: "mysql",
-  port: 3306,
-  logging: false,
-});
-module.exports = sequelize;
+// const { Sequelize } = require("sequelize");
+// const sequelize = new Sequelize("Rishit_Project", "root", "root", {
+//   host: "localhost",
+//   dialect: "mysql",
+//   port: 3306,
+//   logging: false,
+// });
+// module.exports = sequelize;
 
 
 
@@ -13,29 +13,29 @@ module.exports = sequelize;
 ///     MAIN CONNECTION CODE FOR HOSTINGER    ///
 
 
-// const { Sequelize } = require("sequelize");
+const { Sequelize } = require("sequelize");
 
-// const sequelize = new Sequelize(
-//   "u118145129_influencer_db",        // ✅ DB NAME
-//   "u118145129_influencer_use",      // ✅ DB USER
-//   "123!@#Aq",             // ✅ DB PASSWORD
-//   {
-//     host: "auth-db1132.hstgr.io",
-//     dialect: "mysql",
-//     port: 3306,
-//     logging: false,
+const sequelize = new Sequelize(
+  "u118145129_influencer_db",        // ✅ DB NAME
+  "u118145129_influencer_use",      // ✅ DB USER
+  "123!@#Aq",             // ✅ DB PASSWORD
+  {
+    host: "auth-db1132.hstgr.io",
+    dialect: "mysql",
+    port: 3306,
+    logging: false,
 
-//     pool: {
-//       max: 5,
-//       min: 0,
-//       acquire: 30000,
-//       idle: 10000,
-//     },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
 
-//     dialectOptions: {
-//       connectTimeout: 60000,
-//     },
-//   }
-// );
+    dialectOptions: {
+      connectTimeout: 60000,
+    },
+  }
+);
 
-// module.exports = sequelize;   
+module.exports = sequelize;   
